9512b8c8ffd983c067af9e626dead6cbe7610284
jere <jeresanta@gmail.com>
2020-11-07 21:59:54 -0300

    Intentando que funcione

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.use(express.static(__dirname + "/dist/"))
 
 
 app.get(/.*/, function (req,res){
-        res.sendfile(__dirname + "/dist/index.html")
+        res.sendFile(__dirname + "/dist/index.html")
 })
 
 app.listen(port)
