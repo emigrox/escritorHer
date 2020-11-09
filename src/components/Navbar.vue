c0e42335e80ec120eca920779796c26a0c76db69
jere <jeresanta@gmail.com>
2020-11-09 08:21:02 -0300

    Comenzando con Navbar y views

---
+++ src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+      <v-app-bar
+        color="deep-purple accent-4"
+        dense
+        elevate-on-scroll
+        dark
+        app
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  
+        <v-toolbar-title>Page title</v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn icon to="/">
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+  
+        <v-btn icon to="/About">
+          <v-icon>mdi-account-box</v-icon>
+        </v-btn>
+  
+        <v-menu
+          left
+          bottom
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              icon
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+  
+          <v-list>
+            <v-list-item
+              v-for="n in 5"
+              :key="n"
+              @click="() => {}"
+            >
+              <v-list-item-title>Option {{ n }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-app-bar>
+</template>
+
+<script>
+export default {
+  data: () => ({ value: 1 }),
+
+}
+</script>
+
+<style>
+
+</style>
