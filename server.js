c8c978538b5c7e0dc5cc944d77cd4455ebdbd66d
Emiliano Adelar Bade <emilianoobade@gmail.com>
2020-11-09 00:32:42 -0300

    base datos

--- server.js
+++ server.js
@@ -4,19 +4,6 @@ const bodyParser = require('body-parser')
 const port = process.env.PORT || 8080
 
 const app = express()
-
-const connection = mysql.createConnection({
-    host: "us-cdbr-east-02.cleardb.com",
-    user: "b1bc10ad94850e",
-    password: "0245c9f6",
-    database: "heroku_1a05cfe9ca51313"
-})
-
-connection.connect(error => {
-    if(error) throw error
-    console.log('Database server running.')
-}) 
-// mysql://b1bc10ad94850e:0245c9f6@us-cdbr-east-02.cleardb.com/heroku_1a05cfe9ca51313?reconnect=true
  
 
 app.use(express.static(__dirname + "/dist/"))
