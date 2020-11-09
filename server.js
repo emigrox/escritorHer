b33290716dc36074baa8737c761dca9ea0aa286b
jere <jeresanta@gmail.com>
2020-11-08 22:29:59 -0300

    todavia intentando que funcione

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const mysql = require('mysql')
-const bodyParser = requiere('body-Parser')
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 8080
 
 const app = express()
+
 const connection = mysql.createConnection({
     host: "us-cdbr-east-02.cleardb.com",
     user: "b1bc10ad94850e",
@@ -14,7 +15,7 @@ const connection = mysql.createConnection({
 connection.connect(error => {
     if(error) throw error
     console.log('Database server running.')
-})
+}) 
 // mysql://b1bc10ad94850e:0245c9f6@us-cdbr-east-02.cleardb.com/heroku_1a05cfe9ca51313?reconnect=true
  
 
