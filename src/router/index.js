1347c3388e97456b3a67eb396df2ffd7662f9169
jere <jeresanta@gmail.com>
2020-11-08 22:41:42 -0300

    porfavor

--- src/router/index.js
+++ src/router/index.js
@@ -17,12 +17,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
-  },
+  }//,
+  //{
+  //  path: '/login',
+  //  name: 'Login',
+  //  component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+  //}, 
+  /*
   {
     path: '/Signup',
     name: 'Signup',
@@ -32,7 +33,7 @@ const routes = [
     path: '/books',
     name: 'books',
     component: () => import('@/views/books')
-  }
+  }*/
 ]
 
 const router = new VueRouter({
